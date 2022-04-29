e129169da0f310d84258bfc8e3410553f871e9bf
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-04-29 12:52:19 +0300

    new game

---
+++ main.rb
@@ -0,0 +1,17 @@
+require_relative 'lib/console_interface'
+require_relative 'lib/game'
+
+puts 'Всем привет!'
+
+file_path = File.join(__dir__, '/data/words.txt')
+word = File.readlines(file_path, encoding: 'UTF-8', chomp: true).sample
+game = Game.new(word)
+console_interface = ConsoleInterface.new(game)
+
+until game.over?
+  console_interface.print_out
+  letter = console_interface.get_input
+  game.play!(letter)
+end
+
+console_interface.print_out
