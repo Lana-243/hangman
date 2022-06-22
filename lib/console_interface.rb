4e052a41048581fc282501a067685d8cf9d446bb
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-06-22 12:50:06 +0300

    added gemfile

--- lib/console_interface.rb
+++ lib/console_interface.rb
@@ -1,3 +1,5 @@
+require "colorize"
+
 class ConsoleInterface
   FIGURES =
     Dir["#{__dir__}/../data/figures/*.txt"].
@@ -10,16 +12,16 @@ class ConsoleInterface
 
   def print_out
     puts <<~GAME_PROCESS
-      Слово: #{word_to_show}
+      Слово: #{word_to_show.colorize(:light_blue)}
       #{figure}
-      Ошибки (#{@game.errors_made}): #{errors_to_show}
+      Ошибки (#{@game.errors_made}): #{errors_to_show.colorize(:light_magenta)}
       У вас осталось ошибок: #{@game.errors_allowed}
     GAME_PROCESS
 
     if @game.won?
-      puts 'Поздравляем, вы выиграли!'
+      puts 'Поздравляем, вы выиграли!'.colorize(:light_green)
     elsif @game.lost?
-      puts "Вы проиграли, загаданное слово: #{@game.word}"
+      puts "Вы проиграли, загаданное слово: #{@game.word.colorize(:green)}"
     end
   end
 
@@ -36,7 +38,7 @@ class ConsoleInterface
   end
 
   def get_input
-    print 'Введите следующую букву: '
+    print 'Введите следующую букву: '.colorize(:blue)
     gets[0].upcase
   end
 end
